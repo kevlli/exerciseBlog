658ca10ee5aaaddb88e277458f511144e285011b
Kevin Li <kevinlouisli7@gmail.com>
2024-02-07 18:43:50 -0500

    Implemented .env file to hide important api keys and passwords

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-
+import * as dotenv from "dotenv";
 import { accountRouter } from "./routes/accounts.js";
+
+dotenv.config();
+const pswd = process.env.MONGOPSWD;
 
 const app = express();
 
@@ -12,7 +15,7 @@ app.use(cors());
 app.use("/auth", accountRouter);
 
 mongoose.connect(
-  "mongodb+srv://kevlli:fullStackTIME@recipes.8mnryir.mongodb.net/recipes?retryWrites=true&w=majority"
+  `mongodb+srv://kevlli:${pswd}@recipes.8mnryir.mongodb.net/recipes?retryWrites=true&w=majority`
 );
 
 app.listen(3001, () => console.log("Server is up"));
--- backend/src/routes/accounts.js
+++ backend/src/routes/accounts.js
@@ -1,6 +1,7 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import * as dotenv from "dotenv";
 import { accountModel } from "../models/accounts.js";
 
 const router = express.Router();
@@ -16,14 +17,34 @@ router.post("/register", async (req, res) => {
     return res.json({ message: "Username is taken." });
   }
 
-  const hashedPassword = await bcrypt.hash(password, 15);
-  const newAccount = new accountModel({ username, password: hashedPassword });
+  const encryptedPassword = await bcrypt.hash(password, 15);
+  const newAccount = new accountModel({
+    username,
+    password: encryptedPassword,
+  });
   newAccount.save();
   // else, encrypt password and add account details to account database collection
 
   res.json({ message: "User successfully registered." });
 });
 
-router.post("/login");
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const account = await accountModel.findOne({ username });
+  if (!account) {
+    return res.json({ message: "Account not found." });
+  }
+
+  const correctPassword = await bcrypt.compare(password, account.password);
+  if (!correctPassword) {
+    return res.json({ message: "Invalid password." });
+  }
+
+  dotenv.config();
+  const secret = process.env.SECRET;
+  const token = jwt.sign({ id: account._id }, secret);
+  res.json({ token, userID: account._id });
+});
 
 export { router as accountRouter };
