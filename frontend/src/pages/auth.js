51ee3b3974a7409a0da3f618939d92bfed732c61
Kevin Li <kevinlouisli7@gmail.com>
2024-02-07 21:26:48 -0500

    front end routing and login logout features implemented

--- frontend/src/pages/auth.js
+++ frontend/src/pages/auth.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import axios from "axios";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
 export const Auth = () => {
   return (
@@ -9,10 +12,17 @@ export const Auth = () => {
   );
 };
 
-const Form = ({ username, setUsername, password, setPassword, label }) => {
+const Form = ({
+  username,
+  setUsername,
+  password,
+  setPassword,
+  label,
+  onSubmit,
+}) => {
   return (
     <div className="auth-container">
-      <form>
+      <form onSubmit={onSubmit}>
         <h2>{label}</h2>
         <div className="form-class">
           <label htmlFor="username"> Username: </label>
@@ -26,13 +36,13 @@ const Form = ({ username, setUsername, password, setPassword, label }) => {
         <div className="form-class">
           <label htmlFor="password"> Password: </label>
           <input
-            type="text"
+            type="password"
             id="password"
             value={password}
             onChange={(event) => setPassword(event.target.value)}
           />
         </div>
-        <button type="submit">Register</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
@@ -41,6 +51,29 @@ const Form = ({ username, setUsername, password, setPassword, label }) => {
 const Login = () => {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
+
+  const [_, setCookies] = useCookies(["access_token"]);
+
+  const navigate = useNavigate();
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:3001/auth/login", {
+        username,
+        password,
+      });
+      if (response.data.message) {
+        alert(response.data.message);
+      } else {
+        setCookies("access_token", response.data.token);
+        window.localStorage.setItem("userID", response.data.userID);
+        navigate("/");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <Form
       username={username}
@@ -48,6 +81,7 @@ const Login = () => {
       password={password}
       setPassword={setPassword}
       label="Login"
+      onSubmit={onSubmit}
     />
   );
 };
@@ -55,6 +89,19 @@ const Login = () => {
 const Register = () => {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:3001/auth/register", {
+        username,
+        password,
+      });
+      alert(response.data.message);
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <Form
       username={username}
@@ -62,6 +109,7 @@ const Register = () => {
       password={password}
       setPassword={setPassword}
       label={"Register"}
+      onSubmit={onSubmit}
     />
   );
 };
